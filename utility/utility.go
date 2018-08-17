3bcfe65ce9fded859ebee1ff673a63d6b06dba44
Alex <alexmazvasquez@gmail.com>
2018-08-17 12:22:45 -0700

    Utility

---
+++ utility/utility.go
@@ -0,0 +1,22 @@
+package utility
+
+import (
+	"fmt"
+)
+
+func PrintUserName(name string) string {
+	return "Hello " + name
+}
+func Testing() {
+	fmt.Println("Krysta the testing is working ")
+}
+func CreateMap() map[int]string {
+	// Declare and initialize map using map literal
+	names := map[int]string{
+		1: "Aline",
+		2: "Krysta",
+		3: "Samantha",
+		4: "Lisa",
+	}
+	return names
+}
